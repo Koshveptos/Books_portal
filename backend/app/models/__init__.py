1d8bff990ba66f7eb567389487f7af8b9b911bb8
Koshveptos <kazakovvasilij1@gmail.com>
2025-04-21 14:11:44 +0300

    secont try

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,12 @@
+from .base import Base
+from .book import Book, Category, Tag
+from .user import User, UserRole
+
+__all__ = [
+    "Base",
+    "Book",
+    "Category",
+    "Tag",
+    "User",
+    "UserRole",
+]
--- backend/app/models/book.py
+++ backend/app/models/book.py
@@ -1,56 +1,59 @@
 from __future__ import annotations
 
-from datetime import UTC, datetime
+from datetime import datetime
+from typing import List
 
 from sqlalchemy import Column, DateTime, ForeignKey, String, Table
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.models.base import Base
+from .base import Base
 
-book_categories = Table(
-    "books_categories",
+book_category = Table(
+    "book_category",
     Base.metadata,
-    Column("book_id", ForeignKey("books.id")),
-    Column("category_id", ForeignKey("categories.id")),
+    Column("book_id", ForeignKey("book.id", ondelete="CASCADE"), primary_key=True),
+    Column("category_id", ForeignKey("category.id", ondelete="CASCADE"), primary_key=True),
 )
 
-book_tags = Table(
-    "books_tags", Base.metadata, Column("book_id", ForeignKey("books.id")), Column("tag_id", ForeignKey("tags.id"))
+book_tag = Table(
+    "book_tag",
+    Base.metadata,
+    Column("book_id", ForeignKey("book.id", ondelete="CASCADE"), primary_key=True),
+    Column("tag_id", ForeignKey("tag.id", ondelete="CASCADE"), primary_key=True),
 )
 
 
 class Book(Base):
-    # id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(String(50), index=True)
-    author: Mapped[str] = mapped_column(String(255))
-    year: Mapped[str | None] = mapped_column(String(4), index=True)
-    publisher: Mapped[str | None] = mapped_column(String(255), index=True)
-    isbn: Mapped[str] = mapped_column(String(20))
-    description: Mapped[str | None] = mapped_column(String(1023))
-    cover: Mapped[str | None] = mapped_column(String(255))
-    language: Mapped[str | None] = mapped_column(String(50))
-    file_url: Mapped[str] = mapped_column(String(255))
-    # Время создания (устанавливается один раз при создании записи)
-    created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=lambda: datetime.now(UTC), nullable=False
-    )
+    __tablename__ = "book"
 
-    # Время обновления (обновляется при каждом изменении записи)
-    updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), default=lambda: datetime.now(UTC), onupdate=lambda: datetime.now(UTC), nullable=False
-    )
-    # rating: Mapped[Optional[float]] = mapped_column(default=0.0) /
-    # views: Mapped[int] = mapped_column(default=0)
-    categories: Mapped[list[Category]] = relationship(secondary=book_categories, back_populates="books")
-    tags: Mapped[list[Tag]] = relationship(secondary=book_tags, back_populates="books")
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(100))
+    author: Mapped[str] = mapped_column(String(100))
+    description: Mapped[str] = mapped_column(String(1000))
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    # Отношения
+    categories: Mapped[List["Category"]] = relationship(secondary=book_category, back_populates="books")
+    tags: Mapped[List["Tag"]] = relationship(secondary=book_tag, back_populates="books")
 
 
 class Category(Base):
-    name_categories: Mapped[str] = mapped_column(String(50), index=True, nullable=False, unique=True)
-    description: Mapped[str | None] = mapped_column(String(255))
-    books: Mapped[list[Book]] = relationship(secondary=book_categories, back_populates="categories")
+    __tablename__ = "category"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50), unique=True)
+    description: Mapped[str] = mapped_column(String(200))
+
+    # Отношения
+    books: Mapped[List[Book]] = relationship(secondary=book_category, back_populates="categories")
 
 
 class Tag(Base):
-    name_tag: Mapped[str] = mapped_column(String(50), index=True, nullable=False, unique=True)
-    books: Mapped[list[Book]] = relationship(secondary=book_tags, back_populates="tags")
+    __tablename__ = "tag"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30), unique=True)
+
+    # Отношения
+    books: Mapped[List[Book]] = relationship(secondary=book_tag, back_populates="tags")
