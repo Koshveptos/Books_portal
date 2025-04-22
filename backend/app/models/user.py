91aaed3375a8a6f87a095019711bcafc471f4526
Koshveptos <kazakovvasilij1@gmail.com>
2025-04-22 03:20:58 +0300

    add transport

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,6 +1,10 @@
-from fastapi_users.db import SQLAlchemyBaseUserTable
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
 from models.base import Base
+from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
-    pass
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
